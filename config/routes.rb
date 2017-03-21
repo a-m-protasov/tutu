2bd6a85db4526966a3683483da40515ee66033a8
Alexey Protasov <a.m.protasov@gmail.com>
2017-03-21 23:15:34 +0300

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
   resources :trains
   resources :railway_stations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
